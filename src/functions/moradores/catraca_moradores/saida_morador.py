41ebc8c66f0b6d90eb42ab2000a286871071ac11
Pedro Augusto <apedro.rp@gmail.com>
2025-11-28 15:39:35 -0300

    feat: catraca morador

---
+++ src/functions/moradores/catraca_moradores/saida_morador.py
@@ -0,0 +1,24 @@
+from src.utils.input_handler import get_valid_input
+from src.utils.validations import validate_placa, validate_yes_no
+
+def registrar_saida_morador(repositorio):
+    print("\n--- 🛫 Saída de Morador ---")
+    
+    placa_busca, _ = get_valid_input("Digite a placa do morador: ", validate_placa)
+    
+    todos = repositorio.listar_moradores()
+    morador = next((m for m in todos if m.placa == placa_busca), None)
+    
+    if not morador:
+        print("❌ Morador não encontrado.")
+        return
+
+    if not morador.estacionado:
+        print(f"⚠️  O morador {morador.nome} já consta como FORA do condomínio.")
+        return
+
+    try:
+        repositorio.registrar_saida_morador(morador.id)
+        print(f"👋 Até logo, {morador.nome}!")
+    except Exception as e:
+        print(f"❌ Erro ao registrar saída: {e}")
