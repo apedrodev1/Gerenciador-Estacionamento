834caca6c7340d121ae50ed9f9bca387e8429195
Pedro Rocha <apedro.rp@gmail.com>
2026-01-14 10:08:15 -0300

    feat: Colors now is a class

---
+++ src/UI/colors.py
@@ -0,0 +1,13 @@
+"""
+Defines ANSI escape code constants for terminal colors.
+Localização: src/ui/colors.py
+"""
+class Colors:
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    CYAN = "\033[96m"
+    BOLD = "\033[1m"
+    DIM = "\033[2m"
+    RESET = "\033[0m"
