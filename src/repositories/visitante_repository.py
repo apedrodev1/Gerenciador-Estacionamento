97d71c79c130d021156ac85be9f268cddd2241fc
Pedro Rocha <apedro.rp@gmail.com>
2026-02-01 19:40:34 -0300

    fix: import paths

--- src/repositories/visitante_repository.py
+++ src/repositories/visitante_repository.py
@@ -6,7 +6,7 @@ Localização: src/repositories/visitante_repository.py
 """
 from src.repositories.base_repository import BaseRepository
 from src.db import queries
-from src.classes.visitante.Visitante import Visitante
+from src.classes.Visitante.Visitante import Visitante
 
 class VisitanteRepository(BaseRepository):
     
