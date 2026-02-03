63ebd470edf66094ed3ce7a32aa3102536f7e0ab
Pedro Augusto <apedro.rp@gmail.com>
2026-02-03 16:55:08 -0300

    refactor: ui do mapa

--- src/UI/mapa.py
+++ src/UI/mapa.py
@@ -19,36 +19,56 @@ def exibir_mapa_estacionamento(repositorio):
 
     table = Table(box=box.ROUNDED, title="Veículos no Pátio", title_justify="left")
 
-    table.add_column("Vaga", justify="center", style="cyan", no_wrap=True)
+    table.add_column("Local/Vaga", justify="center", style="cyan", no_wrap=True)
     table.add_column("Tipo", style="magenta")
     table.add_column("Motorista", style="white")
     table.add_column("Placa", justify="center", style="green")
     table.add_column("Modelo / Cor", style="dim")
 
-    for item in ocupacao:
-        if item['tipo'] == 'Morador':
-            tipo_fmt = "[bold blue]Morador 🏠[/bold blue]"
-            nome_fmt = f"[bold]{item['nome']}[/bold]"
-        else:
-            tipo_fmt = "[bold yellow]Visitante 🚗[/bold yellow]"
-            nome_fmt = item['nome']
+    count_visitantes = 0
+    count_moradores = 0
 
-        detalhes = f"{item['modelo']} - {item['cor']}"
+    for row in ocupacao: 
+        tipo_db = row['tipo']      # 'MORADOR' ou 'VISITANTE'
+        apto_num = row['apto_num']
+        apto_bloco = row['apto_bloco']
+        vaga_vis = row['vaga_visitante']
+        nome = row['proprietario']
+        placa = row['placa']
+        modelo = row['modelo']
+        cor = row['cor']
+        
+        # Formatação Visual
+        detalhes = f"{modelo} - {cor}"
+
+        if tipo_db == 'MORADOR':
+            count_moradores += 1
+            local_fmt = f"{apto_num}-{apto_bloco}" if apto_bloco else f"{apto_num}"
+            tipo_fmt = "[bold blue]Morador 🏠[/bold blue]"
+            nome_fmt = f"[bold]{nome}[/bold]"
+        
+        elif tipo_db == 'VISITANTE':
+            count_visitantes += 1
+            local_fmt = f"Vaga {vaga_vis}"
+            tipo_fmt = "[bold yellow]Visitante 🚗[/bold yellow]"
+            nome_fmt = nome
+        else:
+            local_fmt = "???"
+            tipo_fmt = "[red]Desconhecido[/red]"
+            nome_fmt = str(nome)
 
         table.add_row(
-            str(item['vaga']),
+            str(local_fmt),
             tipo_fmt,
-            nome_fmt,
-            item['placa'],
-            detalhes
+            str(nome_fmt),
+            str(placa),
+            str(detalhes)
         )
 
     console.print(table)
 
-    total = len(ocupacao)
-    visitantes = sum(1 for x in ocupacao if x['tipo'] == 'Visitante')
-    moradores = total - visitantes
+    total = count_moradores + count_visitantes
     
-    console.print(f"\n[dim]Total: {total} | Visitantes: {visitantes} | Moradores: {moradores}[/dim]")
+    console.print(f"\n[dim]Total: {total} | Visitantes: {count_visitantes} | Moradores: {count_moradores}[/dim]")
     
     input(f"\n{Colors.DIM}Pressione Enter para voltar...{Colors.RESET}")
