23cb89fa61b7034e7421bf3fa1aa755e9c0ec711
Pedro Augusto <apedro.rp@gmail.com>
2026-02-03 12:53:54 -0300

    refactor: tudo aponta para .env, ele estabelece nome, qtd... etc.

--- src/utils/setup.py
+++ src/utils/setup.py
@@ -5,9 +5,12 @@ Localização: src/utils/setup.py
 """
 import os
 import sys
+from dotenv import load_dotenv 
 from src.repositories.estacionamento_repository import EstacionamentoRepository
 from src.classes.Estacionamento import Estacionamento
 from src.ui.components import show_error
+
+load_dotenv()
 
 def inicializar_sistema():
     """
@@ -25,18 +28,21 @@ def inicializar_sistema():
         show_error(f"Falha crítica ao conectar no Banco: {e}")
         sys.exit(1)
 
-    # 2. Configurações de Negócio
+    # 2. Configurações de Negócio do Estacionamento
     try:
-        cap = int(os.getenv("TOTAL_CAPACITY", 20))
+        cap_vis = int(os.getenv("CAPACIDADE_VISITANTES", 20))
+        cap_mor = int(os.getenv("CAPACIDADE_MORADORES", 50))
         tempo = int(os.getenv("TIME_LIMIT_MINUTES", 120))
         nome = os.getenv("PARKING_NAME", "Condomínio Solar")
     except ValueError:
         show_error("Erro no .env: Valores numéricos inválidos.")
         sys.exit(1)
 
+    # Cria a instância com os valores lidos
     estacionamento = Estacionamento(
         nome=nome,
-        capacidade_visitantes=cap,
+        capacidade_visitantes=cap_vis,
+        capacidade_moradores=cap_mor, 
         tempo_limite_minutos=tempo
     )
     
