75ef2821c7fe70120053cc2e2e64a3481d60ecff
Pedro Augusto <apedro.rp@gmail.com>
2026-02-04 16:29:46 -0300

    feat: novo sistema de camadas de autorizações

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ Ponto de entrada principal.
 Apenas inicializa e chama o menu UI.
 """
 from dotenv import load_dotenv
+from src.ui.auth import realizar_login
 from src.utils.setup import inicializar_sistema
 from src.ui.menu import executar_menu_principal 
 
@@ -10,11 +11,14 @@ from src.ui.menu import executar_menu_principal
 load_dotenv()
 
 def main():
-    # 1. Preparação (Setup)
+    # 1. Preparação (Setup do Banco e Classes)
     repo, estacionamento = inicializar_sistema()
 
-    # 2. Execução (UI)
-    executar_menu_principal(repo, estacionamento)
+    # 2. Segurança (Login)
+    usuario_logado = realizar_login(repo)
+
+    # 3. Execução (UI)
+    executar_menu_principal(repo, estacionamento, usuario_logado)
 
 if __name__ == "__main__":
     main()
